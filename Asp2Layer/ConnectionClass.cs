e51326327df05e94790e9f80baeb2e2308cb039c
DELL <DELL@DESKTOP-SUSMANN>
2024-10-01 11:56:04 +0530

    Add project files.

---
+++ Asp2Layer/ConnectionClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Asp2Layer
+{
+   public class ConnectionClass
+    {
+        SqlConnection con;//public declaration of connection cls obj & SqlCommand obj
+        SqlCommand cmd;
+        public Connectionclass()
+        {
+            con = new SqlConnection(@"server=DESKTOP-JAF6I6Q\SQLEXPRESS;database=2Layer;Integrated Security=true")
+
+        }
+        public int Fn-ExeNonqry(string sqlqry)//insert,dlt,update---ExecuteNonQry--returns int value
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+
+            }
+            cmd = new SqlCommand(sqlqry, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+        public string Fn_Exescalar(string sqlqry)//select,aggragte fns--returns string value
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            cmd = new SqlCommand(sqlqry, con);
+            con.Open();
+            string s = cmd.ExecuteScalar().ToString();
+            con.Close();
+            return s;
+        }
+        public SqlDataReader Fn_ExeReader(string sqlqry)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            cmd = new SqlCommand(sqlqry, con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            return dr;
+        }
+        public DataSet Fn ExeDataAdapter(string salary)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlqry, con);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+        public DataTable Fn_ExeDataTable(string sqlqry)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            SqlDataAdapter da = new SqlDataAdapter(sqlqry, con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
---
+++ Asp2Layer/WebForm2.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="Asp2Layer.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <p>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ADMIN&nbsp;</p>
+    <p>
+        Admin name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </p>
+</body>
+</html>
